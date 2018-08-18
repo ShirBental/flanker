7630d8d16cbb6c8bc5f9857a40a0f4e2a52b5574
shirBental <shir6shir@gmail.com>
2018-08-18 20:44:50 +0300

    added save file and form validation

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000
+const fs=require('fs');
+
+var resFile='message.txt';
+var writeToFile=function(req,res) {
+    console.log(req,res);
+    console.log(req.body);
+    var output=req.body;
+    fs.appendFile(resFile, JSON.stringify(output)+'\n', function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+    res.send('OK');
+
+}
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use('/bower_components',express.static(path.join(__dirname, 'bower_components')))
-  .get('/', (req, res) => res.sendFile(path.join(__dirname,'index.html')))
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: true }))
+.get('/', (req, res) => res.sendFile(path.join(__dirname,'index.html')))
+    .get('/download', function(req, res){
+        res.download(resFile);
+    })
+  .post('/results', (req, res) => writeToFile(req,res))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
