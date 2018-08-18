577ada1cad3173bc62b9ef35eec43493f5624d2e
shirBental <shir6shir@gmail.com>
2018-08-18 18:42:27 +0300

    client side prototype

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use('/bower_components',express.static(path.join(__dirname, 'bower_components')))
   .get('/', (req, res) => res.sendFile(path.join(__dirname,'index.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
